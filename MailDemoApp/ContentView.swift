520a5c4d3821c141b74259c52449ce31467668c0
Starsky <starsky.m@live.com>
2022-06-30 12:32:32 +0200

    Initial Commit

---
+++ MailDemoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MailDemoApp
+//
+//  Created by star on 30.06.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MailDemoApp/MailDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MailDemoAppApp.swift
+//  MailDemoApp
+//
+//  Created by star on 30.06.22.
+//
+
+import SwiftUI
+
+@main
+struct MailDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
